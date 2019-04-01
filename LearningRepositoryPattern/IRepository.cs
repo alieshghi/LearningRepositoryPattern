ffee835ebefc3954527266f9ddb61b8052ffa74c
AliEshghi <a.eshghi94@gmail.com>
2019-04-01 16:21:45 +0430

    Add project files.

---
+++ LearningRepositoryPattern/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+
+namespace LearningRepositoryPattern
+{
+    interface IRepository <TEntity> where TEntity :class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity>> predicate);
+
+        void Add(TEntity entities);
+        void AddRange(List<TEntity> entities);
+
+        void Remove(TEntity entities);
+        void RemoveRange(TEntity entities);
+
+    }
+}
---
+++ LearningRepositoryPattern/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.e
+
+namespace LearningRepositoryPattern
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public void Add(TEntity entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(List<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(TEntity entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
